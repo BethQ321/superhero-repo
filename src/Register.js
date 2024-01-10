9642494ebc974004b6fd7a7f6571793c861016ea
Krieg <krieg@Krieg-Macbook-Pro.local>
2024-01-10 14:23:17 -0500

    added email, first, last name, phone to user database

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import {React, UseState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from "axios";
+
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const[Fname, setFirstName] = useState('');
+  const[Lname, setLastName] = useState('');
+  const[email, setEmail] = useState('');
+  const[phone, SetPhone] = useState('');
+
+
+
     return (
       <div>
         <p>Register stuff goes here</p>
