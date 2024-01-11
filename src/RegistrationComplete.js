e4a8cd25f90f006144213a054a4eebbf1eabbc44
Krieg <krieg@Krieg-Macbook-Pro.local>
2024-01-11 14:00:08 -0500

    fixed registration page

--- src/RegistrationComplete.js
+++ src/RegistrationComplete.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const RegistrationComplete = () => {
   return (
     <div>
-      <p>Registration Complete</p>
+       <h1>Registration Complete</h1>
+                <img src="https://i.makeagif.com/media/2-20-2016/C-jGQN.gif"></img>
+                <h3>Please, <Link to="/Login">Login!</Link></h3>
     </div>
   );
 };
