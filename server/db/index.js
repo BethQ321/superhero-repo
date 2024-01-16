8c46f0f285c04695318a8aef6d22fa40b2a23cdd
jonas <jonasabelmellado@gmail.com>
2024-01-15 20:57:39 -0500

    updated products -> added products, changed most photos to devins photos in imgur, added a class to product table for possible future use

--- server/db/index.js
+++ server/db/index.js
@@ -28,7 +28,8 @@ const seed = async () => {
       price INTEGER,
       image VARCHAR(1000) NOT NULL,
       description VARCHAR(1000),
-      vip_only BOOLEAN DEFAULT false NOT NULL
+      vip_only BOOLEAN DEFAULT false NOT NULL,
+      class VARCHAR (100) NOT NULL
 
     );
 
@@ -172,22 +173,27 @@ const seed = async () => {
       description:
         "Enchanted hammer that grants the wielder (if worthy, no refunds!) control over lightning, flight and superhuman",
       vip_only: true,
+      class:"weapon",
     }),
     createProduct({
-      name: "Vortex Vial",
+      name: "Vortex Vial:",
       price: 100,
-      image: "https://i.imgur.com/Ml3KgVM.png",
+      image:
+        "https://i.imgur.com/Ml3KgVM.png",
       description:
         "A small vial containing a miniature tornado that can be released to create localized storms or tornadoes.",
       vip_only: false,
+      class:"mystic",
     }),
     createProduct({
-      name: "Probability Manipulator Dice",
+      name: "Probability Manipulator Dice:",
       price: 100,
-      image: "https://i.imgur.com/GWTCsuy.png",
+      image:
+        "https://i.imgur.com/GWTCsuy.png",
       description:
         "A set of enchanted dice that can alter the probability of events, allowing the hero to influence luck and outcomes.",
       vip_only: false,
+      class:"mystic",
     }),
     createProduct({
       name: "Shark Laser",
@@ -197,6 +203,7 @@ const seed = async () => {
       description:
         "This is an easily mounted high energy precision laser that fits most big sharks (not hammerheads). Excelent for shark tanks under trap doors in secret layers or castle dikes. ***Batteries and shark not included*** ",
       vip_only: false,
+      class:"weapon",
     }),
     createProduct({
       name: "Lightsaber",
@@ -205,14 +212,16 @@ const seed = async () => {
       description:
         'Perfect weapon for heros or villains who know how to handle a sword, color not guaranteed might change over time based "mood" best if used outdoors, best if user is strong in the force but not a requirement',
       vip_only: false,
+      class:"weapon",
     }),
     createProduct({
-      name: "Spartan_Power_Armor",
+      name: "Power_Armor",
       price: 100,
-      image: "https://m.media-amazon.com/images/I/8180fuwj5GL._AC_SY879_.jpg",
+      image: "https://i.imgur.com/Qdr2Xso.png",
       description:
-        "Heavy duty Power Armor for all non super powered heros out there - performance enhancing and bulletproof and comes with bluetooth and USB-C charging cable",
+        "Heavy duty Power Armor for all non super powered heros comes with a cape for added heroic look ( does not work with jet pack add on )- performance enhancing and bulletproof and comes with bluetooth and USB-C charging cable",
       vip_only: false,
+      class:"suit",
     }),
     createProduct({
       name: "BatRang",
@@ -221,55 +230,62 @@ const seed = async () => {
       description:
         "A bat-shaped throwing weapon used by Batman. It can be thrown at enemies or used for various utility purposes.",
       vip_only: false,
+      class:"weapon",
     }),
     createProduct({
       name: "Webshooter",
       price: 100,
-      image: "https://m.media-amazon.com/images/I/41UQC-jh7vL._AC_US40_.jpg",
+      image: "https://i.imgur.com/Djw9jqJ.png",
       description:
-        "Wrist-mounted devices that allow Spider-Man to shoot and swing from webs. They are a crucial tool for his acrobatic crime-fighting.",
+        "Wrist-mounted devices that allowed Spider-Man to shoot and swing from webs. They are a crucial tool for his acrobatic crime-fighting. Comes in different colors and customizable webs and materials that suits your task",
       vip_only: false,
+      class:"tech",
     }),
     createProduct({
       name: "Jet_Pack",
       price: 100,
-      image: "https://i.ebayimg.com/images/g/fzAAAOSwNAlkgO0d/s-l140.jpg",
+      image: "https://i.imgur.com/OQQzPt4.png",
       description:
-        "Allows the wearer a 2 hour flight time, to escape or infiltrate the most dificicult situations imaginable... or just to make a cool entrance",
+        "Allows the wearer up to 2 hour flight time, to escape or infiltrate the most dificicult situations imaginable... or just to make a cool entrance. Not reccomended with outfits that include capes or highly flammable materials like flannel. Runs on jetfuel or in emergencies 95 octain or higer ",
       vip_only: false,
+      class:"tech",
     }),
     createProduct({
       name: "Gravity_Boots",
       price: 100,
-      image: "https://gravitec.com/wp-content/uploads/2016/06/Boot-White.jpg",
+      image: "https://i.imgur.com/46pKPqE.png",
       description:
-        "Boots with adjustable gravity manipulation, allowing the hero to walk on walls, ceilings, or make impressive leaps. This gadget would provide enhanced mobility and escape options.",
+        "Boots with adjustable gravity manipulation, allowing the hero to walk on walls, ceilings, or make impressive leaps. This gadget would provide enhanced mobility and escape options. Is grated to be used in atmosphere and in space by at least two scientists",
       vip_only: false,
+      class:"tech",
     }),
     createProduct({
-      name: "Stealth_Cloak",
+      name: "Shadow-Distortion Cloak",
       price: 100,
       image:
-        "https://static.bhphoto.com/images/images150x150/1292836852_679301.jpg",
+        "https://i.imgur.com/ENyIxVe.png852_679301.jpg",
       description:
-        "An increadibly hard to see adaptive camouflage cloak that can blend into the surroundings, rendering the hero practically invisible. It could use advanced light-bending technology or even incorporate chameleon-like capabilities.",
+        "A cloak that can manipulate light and shadow, allowing the hero to blend into darkness or create illusions. Great for infiltration and the material is super slippery and does not catch on sharp objects, is very silent and is anti bacterial and odor to throw off scent",
       vip_only: false,
+      class:"suit",
     }),
     createProduct({
       name: "Holographic_Projectors",
       price: 100,
-      image: "https://m.media-amazon.com/images/I/41NEyYKajeL._AC_US100_.jpg",
+      image: "https://i.imgur.com/6tXEOGm.png",
       description:
         "Small, portable devices that can project realistic holograms. Heroes could use them for disguise, creating illusions, or communicating with allies.",
       vip_only: false,
+      class:"tech",
     }),
     createProduct({
-      name: "Spray on Kryptonite",
+      name: "Kryptonite_SprayON",
       price: 100,
       image: "https://i.imgur.com/p5XGoXb.png",
       description:
         "Harmless and biodegradable for most, but for that special someone will put them on even then playing field. Just spray any item and let dry for 2 hours before use, Not recomended for direct use due to short range",
       vip_only: false,
+      class:"tech",
     }),
     createProduct({
       name: "Gravitational Singularity Sphere",
@@ -278,86 +294,106 @@ const seed = async () => {
       description:
         "A handheld orb that can create miniature black holes, capable of absorbing or repelling matter in a localized area.",
       vip_only: false,
+      class:"mystic",
     }),
     createProduct({
-      name: "Elemental Fusion Crystal",
+      name: "Elemental Fusion Crystal:",
       price: 100,
       image: "https://i.imgur.com/tO4PWc0.png",
       description:
-        "A gemstone that, when activated, can combine two elements (e.g., fire, water, earth, and air) to create a unique elemental power.",
+        "A gemstone that, when activated, can combine two elements (e.g., fire, water, earth, and air) to create a unique elemental power. Prior knowledge of chemistry is helpful with this mystical artifact as you will probably not be able to familirazie yourself with all possible combinations last minute, in the right hands this is a very powerful tool in the wrong hands it might end up unleashing catostrophic destruction.",
       vip_only: false,
+      class:"mystic",
     }),
     createProduct({
-      name: "Dimensional Anchor Gloves",
+      name: "Submercible vehicle AKA SubMERICA",
       price: 100,
-      image: "https://i.imgur.com/0EcMpsj.png",
+      image: "https://i.imgur.com/FoUEBpM.png",
       description:
-        "Gloves that can create stable dimensional anchors, preventing teleportation or interdimensional movement in a specific area.",
+        "An aquatic vehicle equipped with sonar, torpedoes, and advanced navigation for underwater missions. Can be used as forward operation command post and a temporary secret layer. ",
       vip_only: false,
+      class:"vehicle",
     }),
     createProduct({
-      name: "Bio-Nano Hive Armor (Men)",
+      name: "Atlas - Giant mech suite",
       price: 100,
-      image: "https://i.imgur.com/83NB7MB.png",
+      image: "https://i.imgur.com/NPHeXeW.png",
       description:
-        "A suit made of nanobots that can rapidly adapt to incoming threats, forming protective shields or augmenting the hero's abilities.",
+        "A colossal ( empire state height ) mechanical suit piloted by the hero, capable of taking on colossal threats or rescuing civilians from danger. Remote piloting possible but not reccomended as it still in Beta testing and could cause catostrophic disctruction of civilian infrastructure. This is not a road legal or axle bearing vehicle and should not be used on infrastucture and or civilan areas unless in absolout last case end of the world scenarios",
       vip_only: false,
+      class:"vehicle",
     }),
     createProduct({
-      name: "Bio-Nano Hive Armor (Women)",
+      name: "Reacto spacecraft",
       price: 100,
-      image: "https://i.imgur.com/XI1Z4XY.png",
+      image: "https://i.imgur.com/QLAY7Za.png",
       description:
-        "A suit made of nanobots that can rapidly adapt to incoming threats, forming protective shields or augmenting the hero's abilities.",
+        "A spacecraft equipped for interstellar travel, enabling the hero to protect Earth from extraterrestrial threats. Can operate in atmosphere and gravity its main purpos is for last minuite space missions, comes equipped with 1 years rations of food and diplomatic material for 'first contact' scenarios",
       vip_only: false,
+      class:"vechicle",
+    }), 
+    createProduct({
+      name: "Terraforming device",
+      price: 100,
+      image: "https://i.imgur.com/pxxAw9P.png",
+      description:
+        "A device that can manipulate and reshape the environment, useful for disaster relief or combating environmental threats. Please note that any hostile intention of use of this device will result in immideate shut down of device and all payments are non refundable",
+      vip_only: false,
+      class:"tech",
     }),
     createProduct({
-      name: "Mind Shielding Helmet",
+      name: "Time bubble Gun",
       price: 100,
-      image: "https://i.imgur.com/nEr4lpb.png",
+      image: "https://i.imgur.com/5l3r7i8.png",
       description:
-        "A helmet that provides protection against mental attacks, such as telepathy or mind control.",
+        "A time manipulaton device that emits a time destortion buble around the user. Once used the device has the ability to slow down time within that bubble and the range is adjustable, bigger the radius the less time distortion available... bcause sicence. An effective tool or defense against speedsters and mystics. ps. a sidenote everything in the bubble will slow down except for the user him self or anyone who is in direct contact and remains in contact with the user upon activation. Bullets that enter the field will slow down allowing the user to reinact the famous Matrix sceane. ",
       vip_only: false,
+      class:"tech",
     }),
     createProduct({
-      name: "Healing Serum",
+      name: " Eye of Heimdall - Astral Projection Amulet",
       price: 100,
-      image: "https://i.imgur.com/tfCU69G.png",
+      image: "https://i.imgur.com/kXSJe8T.png",
       description:
-        " A special serum that accelerates the hero's healing process, aiding in recovery from injuries.",
+        "An amulet that allows the hero to project their consciousness into the astral plane, exploring other realms and gathering information in the way very few can, knowledge is power.",
       vip_only: false,
+      class:"mystic",
     }),
     createProduct({
-      name: "Utility Belt",
+      name: "Gemini - Soul-Linking Bracelet",
       price: 100,
-      image: "https://i.imgur.com/bkdnGNq.png",
+      image: "https://i.imgur.com/E14Z6ud.png",
       description:
-        "A versatile belt with compartments to store various gadgets, tools, and weapons.",
+        " A bracelet that links the hero's soul with another individual, enabling telepathic communication and sharing of experiences. Allows the master werear to learn the truth even faster and with greater accuracy than with the Lasso of truth",
       vip_only: false,
+      class:"mystic",
     }),
     createProduct({
-      name: "Morphing Liquid Chain",
+      name: "Dreamwalker - Enchanted Dreamcatcher",
       price: 100,
-      image: "https://i.imgur.com/Q8cL8Uf.png",
+      image: "https://i.imgur.com/oGCNqAR.png",
       description:
-        "A chain made of shape-shifting liquid metal that can transform into various weapons or tools on command.",
-      vip_only: false,
+        "A dreamcatcher crown that allows the user to manipulate dreams of others. The hero will have the ability to directly affect user through sleepwalking or implanting thoughts or ideas in the short term. Long term affects have been disabled due to dictatorial and world domination tendancies and corruption. VIP only",
+      vip_only: true,
+      class:"mystic",
     }),
     createProduct({
-      name: "Dreamscape Goggles",
+      name: "Mistwalker - Enchanted cape",
       price: 100,
-      image: "https://i.imgur.com/nOFjF8c.png",
+      image: "https://i.imgur.com/lBCL7Nl.jpg",
       description:
-        "Goggles that allow the hero to enter and interact with the dream world, influencing the subconscious minds of others.",
-      vip_only: false,
+        "Mystic Enchanted Cape: A cape imbued with enchantments that grant the hero the ability to phase through solid objects and become incorporeal.",
+      vip_only: true,
+      class:"suit",
     }),
-    createProduct({
-      name: "Reality-Warping Kaleidoscope",
-      price: 100,
-      image: "https://i.imgur.com/qfPO5LP.png",
-      description:
-        "A kaleidoscope that can twist and distort reality, creating mesmerizing visual effects and illusions.",
-      vip_only: false,
+      createProduct({
+        name: "Dreamscape Goggles",
+        price: 100,
+        image: "https://i.imgur.com/nOFjF8c.png",
+        description:
+          "Goggles that allow the hero to enter and interact with the dream world, influencing the subconscious minds of others.",
+        vip_only: false,
+        class: "tech",
     }),
     createProduct({
       name: "Dimensional Resonance Whistle",
@@ -366,6 +402,88 @@ const seed = async () => {
       description:
         "A whistle that can resonate with alternate dimensions, summoning creatures or allies from parallel worlds.",
       vip_only: false,
+      class:"mystic",
+    }),
+    createProduct({
+      name: "Morphing Liquid Chain",
+      price: 100,
+      image: "https://i.imgur.com/Q8cL8Uf.png",
+      description:
+        "A chain made of shape-shifting liquid metal that can transform into various weapons or tools on command.",
+      vip_only: false,
+      class:"weapon",
+    }),
+    createProduct({
+      name: "Utility Belt",
+      price: 100,
+      image: "https://i.imgur.com/bkdnGNq.png",
+      description:
+        "A versatile belt with compartments to store various gadgets, tools, and weapons.",
+      vip_only: false,
+      class:"suit",
+    }),
+    createProduct({
+      name: "Elemental Fusion Crystal",
+      price: 100,
+      image: "https://i.imgur.com/tO4PWc0.png",
+      description:
+        "A gemstone that, when activated, can combine two elements (e.g., fire, water, earth, and air) to create a unique elemental power.",
+      vip_only: false,
+      class:"mystic",
+    }),
+    createProduct({
+      name: "Dimensional Anchor Gloves",
+      price: 100,
+      image: "https://i.imgur.com/0EcMpsj.png",
+      description:
+        "Gloves that can create stable dimensional anchors, preventing teleportation or interdimensional movement in a specific area.",
+      vip_only: false,
+      class:"tech",
+    }),
+    createProduct({
+      name: "Bio-Nano Hive Armor (Men)",
+      price: 100,
+      image: "https://i.imgur.com/83NB7MB.png",
+      description:
+        "A suit made of nanobots that can rapidly adapt to incoming threats, forming protective shields or augmenting the hero's abilities.",
+      vip_only: false,
+      class:"suit",
+    }),
+    createProduct({
+      name: "Bio-Nano Hive Armor (Women)",
+      price: 100,
+      image: "https://i.imgur.com/XI1Z4XY.png",
+      description:
+        "A suit made of nanobots that can rapidly adapt to incoming threats, forming protective shields or augmenting the hero's abilities.",
+      vip_only: false,
+      class:"suit",
+    }),
+    createProduct({
+      name: "Mind Shielding Helmet",
+      price: 100,
+      image: "https://i.imgur.com/nEr4lpb.png",
+      description:
+        "A helmet that provides protection against mental attacks, such as telepathy or mind control.",
+      vip_only: false,
+      class:"tech",
+    }),
+    createProduct({
+      name: "Healing Serum",
+      price: 100,
+      image: "https://i.imgur.com/tfCU69G.png",
+      description:
+        " A special serum that accelerates the hero's healing process, aiding in recovery from injuries.",
+      vip_only: false,
+      class:"tech",
+    }),
+    createProduct({
+      name: "Reality-Warping Kaleidoscope",
+      price: 100,
+      image: "https://i.imgur.com/qfPO5LP.png",
+      description:
+        "A kaleidoscope that can twist and distort reality, creating mesmerizing visual effects and illusions. It casts a wide web and affects as big of an area as the hero can muster, be careful this item is powerfull but using it will come at a cost (other than our store price) to the user as it feeds of the sanity of the user, the more you will loose your sanity so use it wisely and sparsly",
+      vip_only: false,
+      class:"tech",
     }),
     createProduct({
       name: "",
@@ -373,6 +491,7 @@ const seed = async () => {
       image: "",
       description: "",
       vip_only: false,
+      class: "",
     }),
   ]);
   let orders = await fetchOrders(ethyl.id);
