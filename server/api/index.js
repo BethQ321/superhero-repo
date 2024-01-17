94ef43a89ea20c09143bf345e1ab1dfddb6f7d92
Matthew Krieg <121915687+someguy919@users.noreply.github.com>
2024-01-17 12:39:17 -0500

    added wishlist route

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,7 @@ app.use("/orders", require("./orders"));
 app.use("/lineItems", require("./lineItems"));
 app.use("/reviews", require("./reviews"));
 app.use("/users", require("./users"));
-
+app.use("/wishlist", require("./wishlist")); 
 
 //A bunch of stuff to get the error handling to work that I dont fully understand 
 app.use((err, req, res, next) => {
