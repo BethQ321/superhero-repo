4ea54e4322253e62011d2f74db2c7c27fb67572e
someguy <emaiL>
2024-01-22 15:48:03 -0500

    reviews are better

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
-import api from "./api";
+import api from "./api/index";
 
 const SingleProduct = ({
   auth,
@@ -15,17 +15,19 @@ const SingleProduct = ({
 }) => {
   const params = useParams();
   const productId = params.id;
-  const [review, setReview] = useState({
+
+  const [reviewForm, setReviewForm] = useState({
     name: "",
     product_id: productId,
     review_title: "",
     reviewText: "",
     rating: "",
   });
+
   const [reviews, setReviews] = useState([]);
+  const [submissionMessage, setSubmissionMessage] = useState("");
   const [error, setError] = useState(null);
 
-  // Reload fix
   useEffect(() => {
     const fetchReviews = async () => {
       try {
@@ -49,23 +51,24 @@ const SingleProduct = ({
   const handleReviewSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await api.createReview(productId, review);
+      const response = await api.createReview(productId, reviewForm);
       if (response.data) {
         setReviews([...reviews, response.data]);
-        // Reset the review form
-        setReview({
+        setReviewForm({
           name: "",
+          product_id: productId,
           review_title: "",
           reviewText: "",
           rating: "",
         });
+        setSubmissionMessage("Thank you for submitting your review.");
       }
     } catch (error) {
       console.error("Review submission error:", error);
       setError(error.message);
     }
   };
-  
+
   if (!oneProduct) {
     return <div>Loading product...</div>;
   }
@@ -94,90 +97,85 @@ const SingleProduct = ({
       </div>
       <br />
       <Link to="/products">Back to Products</Link>
-      <h2  style={{ textAlign: "center" }}>Write A Review </h2>
-      <form onSubmit={handleReviewSubmit}>
-        <div>
-          <label htmlFor="name">Reviewer:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={review.name}
-            onChange={(e) => setReview({ ...review, name: e.target.value })}
-            required
-          />
-        </div>
-
-        <div>
-          <label htmlFor="reviewTitle">Review Title:</label>
-          <input
-            type="text"
-            id="reviewTitle"
-            name="reviewTitle"
-            value={review.review_title}
-            onChange={(e) =>
-              setReview({ ...review, review_title: e.target.value })
-            }
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="reviewText">Review:</label>
-          <textarea
-            id="reviewText"
-            name="reviewText"
-            value={review.reviewText}
-            onChange={(e) =>
-              setReview({ ...review, reviewText: e.target.value })
-            }
-            required
-            style={{ marginBottom: '10px', width: "100%", height: '100px' }} 
-          />
-        </div>
-        <div>
-  <label htmlFor="rating">Rating (0-5):</label>
-  <select
-    id="rating"
-    name="rating"
-    value={review.rating}
-    onChange={(e) =>
-      setReview({ ...review, rating: parseInt(e.target.value) })
-    }
-    style={{ fontSize: '16px', width: '100px', textAlign: "center", paddingTop: '2px', paddingBottom: '2px' }} 
-  >
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-  </select>
-</div>
-<div style={{ display: 'flex', justifyContent: 'center' }}>
-  <button type="submit" style={{ width: '150px', marginTop: '10px' }}>Submit Review</button> 
-</div>
-      </form>
+      <h2 style={{ textAlign: "center" }}>Write A Review</h2>
+      {!submissionMessage && (
+        <form onSubmit={handleReviewSubmit}>
+          <div>
+            <label htmlFor="name">Reviewer:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={reviewForm.name}
+              onChange={(e) => setReviewForm({ ...reviewForm, name: e.target.value })}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="reviewTitle">Review Title:</label>
+            <input
+              type="text"
+              id="reviewTitle"
+              name="reviewTitle"
+              value={reviewForm.review_title}
+              onChange={(e) => setReviewForm({ ...reviewForm, review_title: e.target.value })}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="reviewText">Review:</label>
+            <textarea
+              id="reviewText"
+              name="reviewText"
+              value={reviewForm.reviewText}
+              onChange={(e) => setReviewForm({ ...reviewForm, reviewText: e.target.value })}
+              required
+              style={{ marginBottom: '10px', width: "100%", height: '100px' }} 
+            />
+          </div>
+          <div>
+            <label htmlFor="rating">Rating (0-5):</label>
+            <select
+              id="rating"
+              name="rating"
+              value={reviewForm.rating}
+              onChange={(e) => setReviewForm({ ...reviewForm, rating: parseInt(e.target.value) })}
+              style={{ fontSize: '16px', width: '100px', textAlign: "center", paddingTop: '2px', paddingBottom: '2px' }} 
+            >
+              <option value="">Select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button type="submit" style={{ width: '150px', marginTop: '10px' }}>Submit Review</button> 
+          </div>
+        </form>
+      )}
+      {submissionMessage && <div style={{ color: 'green', marginTop: '10px' }}>{submissionMessage}</div>}
 
       <div>
         <h3>Reviews</h3>
         <ul className="reviews-list">
-          {reviews
-            .filter((review) => review.product_id === productId)
-            .map((review) => (
-              <li key={review.id} className="review-box">
-                <div className="review-title">
-                  <h4>{review.review_title}</h4>
-                </div>
-                <div className="name">
-                  <p>Review By: {review.name}</p>
-                </div>
-                <div className="review-rating">
-                  <p>Rating: {review.rating}/5</p>
-                </div>
-                <div className="review-text">
-                  <p>Review: {review.reviewtext}</p>
-                </div>
-              </li>
-            ))}
+          {reviews.map((review) => (
+            <li key={review.id} className="review-box">
+              <div className="review-title">
+                <h4>{review.review_title}</h4>
+              </div>
+              <div className="name">
+                <p>Review By: {review.name}</p>
+              </div>
+              <div className="review-rating">
+                <p>Rating: {review.rating}/5</p>
+              </div>
+              <div className="review-text">
+                <p>Review: {review.reviewText}</p>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
